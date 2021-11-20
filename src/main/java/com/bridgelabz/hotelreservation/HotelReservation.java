1e8ecbb46659de84bf285d493ed095f6d3cf5e83
Subhamdas20 <subham21.sd@gmail.com>
2021-11-20 20:39:42 +0530

    Added : uc2 find cheapest hotel

--- src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
+++ src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
@@ -1,22 +1,92 @@
 package com.bridgelabz.hotelreservation;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HotelReservation {
     private List<Hotels> hotelList;
 
-    public HotelReservation()
-    {
+    public HotelReservation() {
         this.hotelList = new ArrayList<Hotels>();
     }
-
-    public void addHotel(Hotels hotel)
-    {
+/**
+ * @param hotel add  used to add hotels to list
+ * */
+    public void addHotel(Hotels hotel) {
         this.hotelList.add(hotel);
     }
-    public List<Hotels> getHotels()
-    {
+/*
+* get list of hotels
+* */
+    public List<Hotels> getHotels() {
         return this.hotelList;
+    }
+
+    public Map<String, Integer> searchFor(String date1, String date2) {
+        int totalDays = totalNumberOfDays(date1, date2);
+        int weekDays = totalNumberOfWeekDays(date1, date2);
+        int weekendDays = totalDays - weekDays;
+        return getCheapestHotels(weekDays, weekendDays);
+    }
+/**
+ * get cheapest hotel
+ * */
+    public Map<String, Integer> getCheapestHotels(int weekDays, int weekendDays)
+    {
+        Map<String, Integer> hotelCosts = new HashMap<>();
+        Map<String, Integer> sortedHotelCosts = new HashMap<>();
+        if (hotelList.size() == 0)
+            return null;
+        this.hotelList.stream().forEach(
+                n -> hotelCosts.put(n.getName(),(n.getRegularWeekdayRate() * weekDays + n.getRegularWeekendRate() * weekendDays)));
+        Integer cheap = hotelCosts.values().stream().min(Integer::compare).get();
+        hotelCosts.forEach((k, v) -> {
+            if (v == cheap)
+                sortedHotelCosts.put(k, v);
+        });
+
+        return sortedHotelCosts;
+    }
+
+
+
+
+/**
+ * count total number of days
+ * @param date1 is start date
+ * @param date2 is end date
+ * */
+    public int totalNumberOfDays(String date1, String date2) {
+        LocalDate startDate = toLocalDate(date1);
+        LocalDate endDate = toLocalDate(date2);
+        int totalDays = Period.between(startDate, endDate).getDays() + 1;
+        return totalDays;
+    }
+/**
+ * use to get total number of week days in booking dates
+ */
+    public int totalNumberOfWeekDays(String date1, String date2) {
+        LocalDate startDate = toLocalDate(date1);
+        LocalDate endDate = toLocalDate(date2);
+        DayOfWeek startDay = startDate.getDayOfWeek();
+        DayOfWeek endDay = endDate.getDayOfWeek();
+        int days = (int) (ChronoUnit.DAYS.between(startDate, endDate) + 1);
+        int daysWithoutWeekends = days - 2 * ((days + startDay.getValue()) / 7);
+        int totalWeekDays = (int) daysWithoutWeekends + (startDay == DayOfWeek.SUNDAY ? 1 : 0)
+                + (endDay == DayOfWeek.SUNDAY ? 1 : 0);
+        return totalWeekDays;
+    }
+
+    public LocalDate toLocalDate(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
+        LocalDate localDate = LocalDate.parse(date, formatter);
+        return localDate;
     }
 }
--- src/test/java/com/bridgelabz/testhotelreservation/TestHotelReservation.java
+++ src/test/java/com/bridgelabz/testhotelreservation/TestHotelReservation.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class TestHotelReservation {
 
@@ -22,6 +23,21 @@ public class TestHotelReservation {
         List<Hotels> hotels = Arrays.asList(hotel1,hotel2,hotel3);
         List<Hotels> result = reserve.getHotels();
         Assert.assertEquals(result,hotels);
+    }
+
+
+    @Test
+    public void givenListOfHotelsAndPriceListWhenDatesGivenShouldreturnCheapestHotel(){
+        Hotels hotel1 = new Hotels("Lakeewood", 110, 90, 80, 80, 3);
+        Hotels hotel2 = new Hotels("Bridgewood", 160, 60, 110, 50, 4);
+        Hotels hotel3 = new Hotels("Ridgewood", 220, 150, 100, 40, 5);
+        HotelReservation reserve = new HotelReservation();
+        reserve.addHotel(hotel1);
+        reserve.addHotel(hotel2);
+        reserve.addHotel(hotel3);
+        Map<String, Integer> result = reserve.searchFor("10Sep2020", "11Sep2020");
+        result.forEach((k, v) -> System.out.println(k+ " " + v));
+
 
     }
 }
